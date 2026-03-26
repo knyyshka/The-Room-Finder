980df8d5b8407315b6417e5736e96c2b515e5f85
Kanishka Gupta <kanshkaa9@gmail.com>
2026-03-26 12:33:06 +0530

    Create data.js with room data for PGs in Subhash Nagar

---
+++ data.js
@@ -0,0 +1,25 @@
+const roomData = [
+    {
+        "name": "PG 1",
+        "address": "Subhash Nagar, Delhi",
+        "price": 15000,
+        "amenities": ["WiFi", "TV", "AC"],
+        "available": true
+    },
+    {
+        "name": "PG 2",
+        "address": "Subhash Nagar, Delhi",
+        "price": 12000,
+        "amenities": ["WiFi", "Laundry"],
+        "available": false
+    },
+    {
+        "name": "PG 3",
+        "address": "Subhash Nagar, Delhi",
+        "price": 18000,
+        "amenities": ["TV", "AC", "Food"],
+        "available": true
+    }
+];
+
+module.exports = roomData;
